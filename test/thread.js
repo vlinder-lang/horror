783c87bb4aeb46a078d62845be77bc10899c8d7c
Radek Slupik <rightfold@gmail.com>
2015-07-12 15:01:57 +0200

    Implement ldgbl instruction

--- test/thread.js
+++ test/thread.js
@@ -73,6 +73,17 @@ export function testThreadLdctor(test) {
     test.done();
 }
 
+export function testThreadLdgbl(test) {
+    this.globalMap.registerGlobal("unit", Object.create(this.typeLoader.fromDescriptor("T;").prototype));
+    this.moduleLoader.loadModule("thread.ldgbl");
+    const sub = this.globalMap.givenName("thread.ldgbl.main");
+    const thr = new thread.Thread(this.globalMap, this.typeLoader, sub);
+    thr.resume();
+    test.strictEqual(thr.evaluationStack.length, 1);
+    test.strictEqual(thr.evaluationStack[0].type.descriptor, "T;");
+    test.done();
+}
+
 export function testThreadRet(test) {
     this.moduleLoader.loadModule("thread.ret");
     const sub = this.globalMap.givenName("thread.ret.main");
